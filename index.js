7c5eafe5589cdf60c7045ef7a853542ab9b994b7
Andy Wang <ownz.andy@gmail.com>
2017-01-03 14:19:26 -0600

    adding auth

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var utterancesDict = {
 }
 
 var sensiRequest = function(endpoint, cb) {
-  req(process.env.SERVER_URL + '/sensi' + endpoint, function (err, response, body) {
+  req(process.env.SERVER_URL + '/sensi' + endpoint, {'auth': {'user': process.env.USER, 'password': process.env.PASSWORD} }, function (err, response, body) {
     if (!err && response.statusCode == 200) {
       return cb(body)
      } else {
